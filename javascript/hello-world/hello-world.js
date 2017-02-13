d6e4af29301b4520df6bd49b7c375b5a6340be37
Jose Hilario Almeida <josealmeida@gmail.com>
2017-02-13 22:43:33 +0000

    complete javascript ex01 hello-world

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -4,12 +4,14 @@
 // Make sure to look at hello-world.spec.js--that should give you some hints about what is
 // expected here.
 
-var HelloWorld = function() {};
+var HelloWorld = function () {};
 
-HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+HelloWorld.prototype.hello = function (input) {
+  if (input === '') {
+    return 'Hello, World!';
+  } else {
+    return 'Hello, ' + input + '!';
+  }
 };
 
 module.exports = HelloWorld;
