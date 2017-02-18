6f67f5edb0c7b8f997155df97969003e5aeaa7ee
Jose Hilario Almeida <josealmeida@gmail.com>
2017-02-18 18:09:34 +0000

    complete javascript ex02 leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,12 +3,38 @@
 // convenience to get you started writing code faster.
 //
 
-var Year = function() {};
-
-Year.prototype.isLeap = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+var Year = function(input) {
+  this.year = input;
 };
+
+Year.prototype.isLeap = function() {
+
+  if (this.isEvenlyDivisibleBy4()) {
+    if (this.isEvenlyDivisibleBy100()) {
+      if (this.isEvenlyDivisibleBy400()) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
+};
+
+Year.prototype.numberIsFloat = function (n){
+  return Number(n) === n && n % 1 !== 0;
+}
+
+Year.prototype.isEvenlyDivisibleBy4 = function (){
+  return !(this.numberIsFloat(this.year / 4));
+}
+
+Year.prototype.isEvenlyDivisibleBy100 = function (){
+  return !(this.numberIsFloat(this.year / 100));
+}
+
+Year.prototype.isEvenlyDivisibleBy400 = function (){
+  return !(this.numberIsFloat(this.year / 400));
+}
 
 module.exports = Year;
